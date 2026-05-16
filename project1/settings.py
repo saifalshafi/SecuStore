64ceae12036ba077a32247de846d53a53cd07862
Saifalshafi <saefalshafi@gmail.com>
2026-05-17 01:48:06 +0300

    fix settings production config

--- project1/settings.py
+++ project1/settings.py
@@ -134,8 +134,8 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('saefalshafi@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('jspm zkry zrbx iude')
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
